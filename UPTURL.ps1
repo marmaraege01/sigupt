23c6277d754ba30614f56566ed454ee88599dfca
technologicscripts <103482732+technologicscripts@users.noreply.github.com>
2022-11-04 03:14:06 +0300

    Add files via upload

---
+++ UPTURL.ps1
@@ -0,0 +1,3 @@
+# Indirme Modülü
+
+Invoke-WebRequest -Uri "https://updates.signal.org/desktop/signal-desktop-win-5.63.0.exe" -OutFile "$env:TEMP\Omega.7z" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome

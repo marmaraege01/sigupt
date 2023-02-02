5052c10e18723c622d02697f0cff234c234c5723
technologicscripts <103482732+technologicscripts@users.noreply.github.com>
2023-02-03 02:23:34 +0300

    Update UPTURL.ps1

--- UPTURL.ps1
+++ UPTURL.ps1
@@ -1,3 +1,3 @@
 # Indirme Modülü
 
-Invoke-WebRequest -Uri "https://updates.signal.org/desktop/signal-desktop-win-6.4.0.exe" -OutFile "$env:TEMP\Omega.7z" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+Invoke-WebRequest -Uri "https://updates.signal.org/desktop/signal-desktop-win-6.4.1.exe" -OutFile "$env:TEMP\Omega.7z" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome

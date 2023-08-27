d0980d43418a31ce04db291478d1ac71aa6ecdae
technologicscripts <103482732+technologicscripts@users.noreply.github.com>
2023-08-27 19:01:22 +0300

    STARTMENU.ps1

--- STARTMENU.ps1
+++ STARTMENU.ps1
@@ -1,8 +1,8 @@
 function DisplayMenu {
 Write-Host @"
 
---Signal'in En Son Sürümü 6.29.0 Güncellemesi--
-Eklenme tarihi: 23.8.2023
+--Signal'in En Son Sürümü 6.29.1 Güncellemesi--
+Eklenme tarihi: 27.8.2023
 
 KURULAN SÜRÜCÜNÜZÜN HARFİ NEDİR?
 Örneğin D,E,F,G bunlardan birisi olabilir.
